dda39e87d9f6be5de25ec7bf344ac7afd9dcef54
oriento <40637236+oriento@users.noreply.github.com>
2018-06-27 11:10:34 -0500

    added a simple java test application

---
+++ testAppJava.java
@@ -0,0 +1,24 @@
+//import java.util.Scanner;
+public class testAppJava{
+
+public static void main(String[] args) {
+//int input;
+//Scanner k = new Scanner(System.in);
+System.out.println("11 or 22");
+//int input = k.nextInteger();
+System.out.println("input worked");
+
+//if(k=11)
+//{
+//System.out.println("You entered 1");
+//}
+//else if(k=22)
+//{
+//System.out.println("You entered 2");
+//}
+//else 
+//{
+//System.out.println("You can only enter 1 or 2");
+//}
+}
+}
